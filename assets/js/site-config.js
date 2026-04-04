ac9bd32709210adf5276416e621e25a0313267b8
Sun-hong Min <devminsh02@gmail.com>
2026-04-04 21:27:23 +0900

    init

---
+++ assets/js/site-config.js
@@ -0,0 +1,8 @@
+window.SITE_PROFILE = {
+  displayName: 'Sun-hong Min',
+  links: {
+    cv: 'https://drive.google.com/file/d/1azvyG5wazMRT2NqQeSNAhtI7xoIplnws/view',
+    lab: 'https://sites.google.com/view/inslab',
+    github: 'https://github.com/devminsh02'
+  }
+};
